be9566f629a6b4b8630cbeeb21108719c6700f40
neyo1009 <neyo1009@gmail.com>
2026-01-27 14:14:38 +0800

    index.html 載入 lucide 樣式和google font

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';       // 引入Bootstrap 樣式 b
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';  // 引入Bootstrap JS  by 納森 
 import App from "./App"; // 確保路徑是對的
 import "./styles/global.scss"; // 這是最重要的地基
+                               // 原本的 all.scss => 註解 by 納森
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
