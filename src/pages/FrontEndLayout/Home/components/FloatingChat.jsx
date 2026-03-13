79db49a126abdb8d9336193fb907f5408c9a8c2a
James <ohbwgab18@gmail.com>
2026-03-13 19:02:34 +0800

    fix API URL

--- src/pages/FrontEndLayout/Home/components/FloatingChat.jsx
+++ src/pages/FrontEndLayout/Home/components/FloatingChat.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./floating-chat-dogs.scss";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE;
 
 const SIZE_MAP = { S: "小型", M: "中型", L: "大型" };
 const DIET_MAP = { PUPPY: "幼犬", ADULT: "成犬", SENIOR: "熟齡" };
